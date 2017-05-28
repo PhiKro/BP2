0345dee136b16af2ad74d8641eefdc8f4f9f4ea9
GPointExtreme <dominik.geiger@hotmail.com>
2017-05-28 14:59:21 +0200

    Partner eingefügt.

---
+++ jsf-jpa/src/main/java/at/campus02/bp2/mbean/PartnerBean.java
@@ -0,0 +1,133 @@
+package at.campus02.bp2.mbean;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.primefaces.component.column.Column;
+import org.primefaces.component.datatable.DataTable;
+import org.primefaces.event.CellEditEvent;
+
+import at.campus02.bp2.model.Partner;
+import at.campus02.bp2.utils.EntityManagerFactoryProvider;
+
+@ManagedBean
+@SessionScoped
+
+public class PartnerBean {
+	
+	private EntityManager entityManager;
+	
+	private Partner newPartner = new Partner();
+	private List<Partner> partnerList = new ArrayList<Partner>();
+	private List<Partner> selectedPartner;
+	
+
+	
+
+	@PostConstruct
+	public void createEntityManager() {
+		entityManager = EntityManagerFactoryProvider.get().createEntityManager();
+	}
+
+	@PreDestroy
+	public void closeEntityManager() {
+		entityManager.close();
+	}
+	
+	public void loadPartnerFromDB(){
+		partnerList = entityManager.createQuery("from Partner", Partner.class).getResultList();
+	}
+	
+	public void save() {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.merge(newPartner);
+		transaction.commit();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Der Partner " + newPartner.getFirmenname() + " wurde hinzugefuegt" ));
+	}
+	
+	public Partner getNewPartner() {
+		return newPartner;
+	}
+
+	public void setNewPartner(Partner newPartner) {
+		this.newPartner = newPartner;
+	}
+
+	public List<Partner> getPartnerList() {
+		loadPartnerFromDB();
+		return partnerList;
+	}
+
+	public void setPartnerList(List<Partner> partnerList) {
+		this.partnerList = partnerList;
+	}
+	
+	//SelectedUser auswaehlen und loeschen
+	public List<Partner> getSelectedPartner() {
+		return selectedPartner;
+	}
+
+	public void setSelectedUser(List<Partner> selectedPartner) {
+		//this.selectedUser.add(selectedPartner);
+		this.selectedPartner = selectedPartner;
+	}
+	
+	public void deletePartner() {
+		
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		for(Partner u : selectedPartner)
+		{
+			entityManager.remove(u);
+		}
+		transaction.commit();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Der Partner " + newPartner.getFirmenname() + " wurde geloescht" ));
+	}
+	
+    public void onCellEdit(CellEditEvent event) {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+        
+        String currentColumn = ((Column)event.getColumn()).getId();
+        
+        if(newValue != null && !newValue.equals(oldValue)) {
+        	
+        	Partner currentPartner = entityManager.find(Partner.class, Integer.valueOf(event.getRowKey()));
+        	
+        	switch(currentColumn) {
+        	case "partnerFirmenname":
+            	currentPartner.setFirmenname(newValue.toString());
+        		break;
+        	case "partnerStrasse":
+            	currentPartner.setStrasse(newValue.toString());
+        		break;
+        	case "partnerAdresse":
+            	currentPartner.setAdresse(newValue.toString());
+        		break;
+        	case "partnerPlz":
+            	currentPartner.setPlz((int)newValue);
+        		break;
+        	}
+        	
+    		EntityTransaction transaction = entityManager.getTransaction();
+    		transaction.begin();
+    		entityManager.merge(currentPartner);
+    		transaction.commit();
+        	
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
+            FacesContext.getCurrentInstance().addMessage(null, msg);   
+        }
+    }
+}
---
+++ jsf-jpa/src/main/java/at/campus02/bp2/model/Partner.java
@@ -0,0 +1,90 @@
+package at.campus02.bp2.model;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Partner")
+
+public class Partner implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String firmenname;
+
+	private String strasse;
+
+	private String adresse;
+
+	private int plz;
+
+	private String premiumstatus;
+	/*
+	 * static { premiumstatus = new String[2]; premiumstatus[0] = "Nein";
+	 * premiumstatus[1] = "Ja"; }
+	 */
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirmenname() {
+		return firmenname;
+	}
+
+	public void setFirmenname(String firmenname) {
+		this.firmenname = firmenname;
+	}
+
+	public String getStrasse() {
+		return strasse;
+	}
+
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getPlz() {
+		return plz;
+	}
+
+	public void setPlz(int plz) {
+		this.plz = plz;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
+	 public String getPremiumstatus() { return premiumstatus; }
+	  
+	 public void setPremiumstatus(String premiumstatus) {
+	 this.premiumstatus = premiumstatus; 
+	 }
+	 
+
+}
